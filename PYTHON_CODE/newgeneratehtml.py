d6ae6884370758da227445f21c23c8d97b96ab0f
rahul-gaidhane <rahul_gaidhane@vistasoftwares.com>
2018-04-04 19:04:06 +0530

    Module to generate jpeg & thumbnail images

--- PYTHON_CODE/newgeneratehtml.py
+++ PYTHON_CODE/newgeneratehtml.py
@@ -3,15 +3,18 @@ from PIL import Image
 import magic #use "pip3 install python-magic" command to install magic library
 import subprocess
 
-def main(InputDir, OutputDir):
-    if OutputDir == "":
-        OutputDir = os.getcwd() + '/Output'
-    else:
-        OutputDir = OutputDir + '/Output'
+def MakeDir(DirPath):
     try:
-        os.mkdir(OutputDir)
+        os.mkdir(DirPath)
     except FileExistsError:
         pass
+
+def main(InputDir, OutputDir):
+    if OutputDir == "":
+        OutputDir = os.path.join(os.getcwd(), 'Output')
+    else:
+        OutputDir = os.path.join(OutputDir, 'Output')
+    MakeDir(OutputDir)
     InputAnnualDirList = os.listdir(InputDir)
     os.chdir(OutputDir)
     f = open('index.html', 'w')
@@ -23,16 +26,12 @@ def main(InputDir, OutputDir):
     f.write('\t<body>\n')
     f.write('\t\t<h1>List Of Annual Issues</h1>\n')
     for InputAnnualDir in InputAnnualDirList:
-        InputAnnualDirPath = InputDir + '/' + InputAnnualDir
-        OutputAnnualDirPath = OutputDir + '/' + InputAnnualDir
-        try:
-            os.mkdir(OutputAnnualDirPath)
-        except FileExistsError:
-            pass
-        GenJpegImages(InputAnnualDirPath, OutputAnnualDirPath)
-        GenThumbnailImages(InputAnnualDirPath, OutputAnnualDirPath)
+        InputAnnualDirPath = os.path.join(InputDir, InputAnnualDir)
+        OutputAnnualDirPath = os.path.join(OutputDir, InputAnnualDir)
+        MakeDir(OutputAnnualDirPath)
+        GenJpegThumbImages(InputAnnualDirPath, OutputAnnualDirPath)
         GenHtmlFiles(OutputAnnualDirPath)
-        GenMetaFiles(OutputAnnualDirPath)
+        #GenMetaFiles(OutputAnnualDirPath)
         f.write('\t\t<p><a href="%s/AnnualList.html">%s</a></p>\n' % (InputAnnualDir, InputAnnualDir))
     f.write('\t<body>\n')
     f.write('</html>\n')
@@ -42,54 +41,52 @@ def GenMetaFiles(OutputDirPath):
     print("Inside GenMetaFiles:\n")
     print("OutputDirPath = %s" % (OutputDirPath))
 
-def GenJpegImages(ArchiveDirPath, JpegDirPath):
-    print("Inside GenJpegImages:\n")
+def GenJpegThumbImages(ArchiveDirPath, OutputDirPath):
+    print("Inside GenJpegThumbImages:\n")
     print("ArchiveDirPath = %s\n" % (ArchiveDirPath))
-    print("JpegDirPath = %s\n" % (JpegDirPath))
-    JpegDirPath = JpegDirPath + '/JPEG_FILES'
-    try:
-        os.mkdir(JpegDirPath)
-    except FileExistsError:
-        pass
-    ArchiveFileList = os.listdir(ArchiveDirPath)
-    os.chdir(JpegDirPath)
-    for ArchiveFile in ArchiveFileList:
+    print("OutputDirPath = %s\n" % (OutputDirPath))
+    JpegDirPath = os.path.join(OutputDirPath, 'JPEG_FILES')
+    ThumbnailDirPath = os.path.join(OutputDirPath, 'THUMBNAIL_FILES')
+    MakeDir(JpegDirPath)
+    MakeDir(ThumbnailDirPath)
+    ArchiveDirFileList = os.listdir(ArchiveDirPath)
+    for ArchiveFile in ArchiveDirFileList:
         ArchiveFileName = ArchiveFile.split('.')[0]
-        ArchiveFilePath = ArchiveDirPath + '/' + ArchiveFile
-        JpegArchivePath = JpegDirPath + '/' + ArchiveFileName
-        try:
-            os.mkdir(ArchiveFileName)
-        except FileExistsError:
-            pass
-        os.chdir(ArchiveFileName)
-        if GetFileType(ArchiveFilePath) == 'tiff':
+        ArchiveFilePath = os.path.join(ArchiveDirPath, ArchiveFile)
+        JpegMonthDirPath = os.path.join(JpegDirPath, ArchiveFileName)
+        ThumbnailMonthDirPath = os.path.join(ThumbnailDirPath, ArchiveFileName)
+        MakeDir(JpegMonthDirPath)
+        MakeDir(ThumbnailMonthDirPath)
+        if os.path.splitext(ArchiveFilePath)[1] == '.tif':
             img = Image.open(ArchiveFilePath)
             for imgcount in range(1000):
                 try:
                     img.seek(imgcount)
-                    img.save('%s-%d.jpeg' % (ArchiveFileName, imgcount))
                 except EOFError:
                     break
-            os.chdir('..')
-        elif GetFileType(ArchiveFilePath) == 'pdf':
-            JpegArchiveFile = JpegArchivePath + '/' + ArchiveFileName + '.jpeg'
-            print('Inside pdf conversion\nJpegArchiveFile = %s\nArchiveFilePath = %s' % (JpegArchiveFile, ArchiveFilePath))
+                ImageFile = '%s-%d.jpeg' % (ArchiveFileName, imgcount)
+                JpegImagePath = os.path.join(JpegMonthDirPath, ImageFile)
+                img.save(JpegImagePath)
+                newimg = Image.open(JpegImagePath)
+                ThumbnailImagePath = os.path.join(ThumbnailMonthDirPath, ImageFile)
+                newimg.thumbnail((300,500))
+                newimg.save(ThumbnailImagePath)
+        elif os.path.splitext(ArchiveFilePath)[1] == '.pdf':
+            ImageFile = '%s.jpeg' % (ArchiveFileName)
+            JpegImagePath = os.path.join(JpegMonthDirPath, ImageFile)
+            ThumbnailImagePath = os.path.join(ThumbnailMonthDirPath, ImageFile)
             f = open('/dev/null', 'w')
-            subprocess.call(['convert', '-trim', '-density', '200', ArchiveFilePath, '-quality', '100', JpegArchiveFile], stderr=f)
-    #        os.system('convert -trim -density 200 %s -quality 100 %s 2>/dev/null' % (ArchiveFilePath, JpegArchiveFile))
-            os.chdir('..')
+            subprocess.call(['convert', '-trim', '-density', '200', ArchiveFilePath, '-quality', '100', JpegImagePath ], stderr=f)
+            subprocess.call(['convert', '-trim', ArchiveFilePath, '-resize', '300', '-quality', '100', ThumbnailImagePath ], stderr=f)
         else:
             print("FileTypeError : In apropriate File detected\nFilePath = %s" % (ArchiveFilePath))
-
-def GenThumbnailImages(FileDirPath, ThumbnailDirPath):
-#    print("Inside GenThumbnailImages:\n")
-#    print("FileDirPath = %s\n" % (FileDirPath))
-#    print("ThumbnailDirPath = %s\n" % (ThumbnailDirPath))
-
 
 def GenHtmlFiles(OutputDirPath):
     print("Inside GenHtmlFiles:\n")
     print("OutputDirPath = %s\n" % (OutputDirPath))
 
-def GetFileType(FilePath):
-    return (magic.from_file(FilePath, mime=True)).split('/')[1]
+def Makedir(DirPath):
+    try:
+        os.mkdir(DirPath)
+    except FileExistsError:
+        pass
